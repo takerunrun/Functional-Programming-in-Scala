36476f12b6dc9c5e6b8d630d244cd64379d6cded
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2019-04-24 22:50:17 +0900

    exercise ~ 3.19

--- src/main/scala/datastructures/List.scala
+++ src/main/scala/datastructures/List.scala
@@ -34,6 +34,15 @@ object List {
     case Cons(h, t) => Cons(h, append(t, a2))
   }
 
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
+    case Nil => z
+    case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+  }
+
+  def sum2(ints: List[Int]): Int = foldRight(ints, 0)((x, y) => x + y)
+
+  def product2(ds: List[Double]): Double = foldRight(ds, 1.0)((x, y) => x * y)
+
 
   // ---------- Exercise ---------
   def tail[A](l: List[A]): List[A] = l match {
@@ -58,8 +67,70 @@ object List {
     case Cons(h, t) => if (f(h)) dropWhile(t, f) else l
   }
 
+  def init[A](l: List[A]): List[A] = l match {
+    case Nil => Nil
+    case Cons(_, Nil) => Nil
+    case Cons(h, t) => Cons(h, init(t))
+  }
+
+  def length[A](l: List[A]): Int = foldRight(l, 0)((_, x) => x + 1)
+
+  @annotation.tailrec
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
+    case Nil => z
+    case Cons(h, t) => foldLeft(t, f(z, h))(f)
+  }
+
+  def sum3(ns: List[Int]): Int = foldLeft(ns, 0)((x, y) => x + y)
+
+  def product3(ds: List[Double]): Double = foldLeft(ds, 1.0)((x, y) => x * y)
+
+  def length2[A](l: List[A]): Int = foldLeft(l, 0)((x, _) => x + 1)
+
+  def reverse[A](l: List[A]): List[A] = foldLeft(l, List[A]())((as, h) => Cons(h, as))
+
+  def appendViaFoldRight[A](a1: List[A], a2: List[A]): List[A] =
+    foldRight(a1, a2)((h, as) => Cons(h, as))
+
+  def appendViaFoldLeft[A](a1: List[A], a2: List[A]): List[A] =
+    foldLeft(reverse(a1), a2)((as, h) => Cons(h, as))
+
+  def concat[A](l: List[List[A]]): List[A] =
+    foldRight(l, Nil: List[A])(append)
+
+  def add1(ints: List[Int]): List[Int] =
+    foldRight(ints, Nil: List[Int])((h, t) => Cons(h+1, t))
+
+  def doubleToString(ds: List[Double]): List[String] =
+    foldRight(ds, Nil: List[String])((h, t) => Cons(h.toString, t))
+
+  def map[A, B](l: List[A])(f: A => B): List[B] =
+    foldRight(l, Nil: List[B])((h, t) => Cons(f(h), t))
+
+  def filter[A](as: List[A])(f: A => Boolean): List[A] =
+    foldRight(as, Nil: List[A])((h, t) => if (f(h)) Cons(h, t) else t)
+
+  // ------ Answers ------
+  def foldRightViaFoldLeft[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(reverse(l), z)((b, a) => f(a, b))
+
+  def foldRightViaFoldLeft_2[A, B](l: List[A], z: B)(f: (A, B) => B): B =
+    foldLeft(l, (b: B) => b)((g, a) => b => g(f(a, b)))(z)
+
+  def foldLeftViaFoldRight[A,B](l: List[A], z: B)(f: (B,A) => B): B =
+    foldRight(l, (b:B) => b)((a,g) => b => g(f(b,a)))(z)
 
   def main(args: Array[String]): Unit = {
-    println(x)
+    val list = List(1, 2, 3, 4, 5)
+    val list2 = List(2, 4, 6, 8)
+    print(filter(list)(i => i % 2 == 0))
   }
+
+  // ------ Required review -------
+
+//  def foldRightViaFoldLeft
+//  def foldLeftViaFoldRight
+//  def concat
+//  def appendViaFoldRight
+//  def appendViaFoldLeft
 }
