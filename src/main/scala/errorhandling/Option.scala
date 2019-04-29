d225fc31101fd6a26d5b2306cce8aba70474b2f4
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2019-04-29 20:51:20 +0900

    exercise ~ 4.1

--- src/main/scala/errorhandling/Option.scala
+++ src/main/scala/errorhandling/Option.scala
@@ -1,5 +1,53 @@
 package errorhandling
 
-class Option {
+sealed trait Option[+A] {
+  def map[B](f: A => B): Option[B] = this match {
+    case None => None
+    case Some(v) => Some(f(v))
+  }
 
+  def flatMap[B](f: A => Option[B]): Option[B] =
+    map(f) getOrElse None
+
+  def flatMap2[B](f: A => Option[B]): Option[B] = this match {
+    case None => None
+    case Some(v) => f(v)
+  }
+
+
+  def getOrElse[B >: A](default: => B): B = this match {
+    case None => default
+    case Some(v) => v
+  }
+
+  def orElse[B >: A](ob: => Option[B]): Option[B] = this match {
+    case None => ob
+    case _ => this
+  }
+
+  def orElse2[B >: A](ob: => Option[B]): Option[B] =
+    this map (Some(_)) getOrElse ob
+
+  def filter(f: A => Boolean): Option[A] = this match {
+    case Some(v) if(f(v)) => this
+    case _ => None
+  }
+
+  def filter2(f: A => Boolean): Option[A] =
+    flatMap(a => if(f(a)) Some(a) else None)
+}
+case class Some[+A](get: A) extends Option[A]
+case object None extends Option[Nothing]
+
+object Option {
+
+  def mean(xs: Seq[Double]): Option[Double] =
+    if (xs.isEmpty) None
+    else Some(xs.sum / xs.length)
+
+  def variance(xs: Seq[Double]): Option[Double] =
+
+  def main(args: Array[String]): Unit = {
+
+  }
 }
