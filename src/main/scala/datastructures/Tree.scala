418420e26876880646eff1fb34f4f1ba759fd4ac
Kokubo Akihiro <akihiro26melle08cielo@gmail.com>
2019-04-29 17:32:37 +0900

    exercise ~ 3.28

--- src/main/scala/datastructures/Tree.scala
+++ src/main/scala/datastructures/Tree.scala
@@ -1,5 +1,44 @@
 package datastructures
 
-class Tree {
+sealed trait Tree[+A]
+case class Leaf[A](value: A) extends Tree[A]
+case class Branch[A](left: Tree[A], right: Tree[A]) extends Tree[A]
 
+object Tree {
+  def size[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(l, r) => size(l) + size(r) + 1
+  }
+
+  def maximum(tree: Tree[Int]): Int = tree match {
+    case Leaf(v) => v
+    case Branch(l, r) => maximum(l) max maximum(r)
+  }
+
+  def depth[A](tree: Tree[A]): Int = tree match {
+    case Leaf(_) => 1
+    case Branch(l, r) => depth(l) max depth(r)
+  }
+
+  def map[A, B](tree: Tree[A])(f: A => B): Tree[B] = tree match {
+    case Leaf(v) => Leaf(f(v))
+    case Branch(l, r) => Branch(map(l)(f), map(r)(f))
+  }
+
+  def fold[A, B](tree: Tree[A])(f: A => B)(g: (B, B) => B): B = tree match {
+    case Leaf(v) => f(v)
+    case Branch(l, r) => g(fold(l)(f)(g), fold(r)(f)(g))
+  }
+
+  def size2[A](tree: Tree[A]): Int = fold(tree)(_ => 1)((l, r) => l + r + 1)
+
+  def maximum2(tree: Tree[Int]): Int = fold(tree)(v => v)((l, r) => l max r)
+
+  
+
+  def main(args: Array[String]): Unit = {
+    val t = Branch(Branch(Leaf(3), Leaf(1)), Branch(Leaf(5), Leaf(9)))
+    println(size2(t))
+    println(maximum2(t))
+  }
 }
